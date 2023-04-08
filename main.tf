7afa9b8c7117042a1283484b5df7f51557116ab4
Anthony Z <zboralski@users.noreply.github.com>
2023-04-08 11:25:17 +0100

    revert all these secret related changes

--- main.tf
+++ main.tf
@@ -20,12 +20,6 @@ resource "google_project_service" "services" {
   disable_on_destroy = false
 }
 
-data "google_cloud_run_service" "instance" {
-  name     = var.name
-  project  = var.project
-  location = var.location
-}
-
 # Google Cloud Run Service
 resource "google_cloud_run_service" "default" {
   name                       = var.name
@@ -70,19 +64,6 @@ resource "google_cloud_run_service" "default" {
         env {
           name  = "VAULT_API_ADDR"
           value = var.vault_api_addr
-        }
-
-        dynamic "env" {
-          for_each = local.secret_uuids
-          content {
-            name = env.key
-            value_from {
-              secret_key_ref {
-                name = env.value
-                key  = "latest"
-              }
-            }
-          }
         }
 
         resources {
@@ -148,22 +129,4 @@ locals {
 
   # Set the Vault storage bucket name or generate a new one if not provided
   vault_storage_bucket_name = var.vault_storage_bucket_name != "" ? var.vault_storage_bucket_name : "${var.name}-${lower(random_id.vault.hex)}-sb"
-}
-
-resource "random_uuid" "secrets" {
-  for_each = var.env_secrets
-}
-
-locals {
-  # esu == existing_secret_uuids
-  esu_string = lookup(data.google_cloud_run_service.instance.template[0].metadata[0].annotations, "run.googleapis.com/secrets", "")
-  esu_parts = local.esu_string == ""? []: split(",", local.esu_string)
-  # Key should be the second part, value should be the first part
-  esu_values = local.esu_string == ""? [] : [for part in local.esu_parts : split(":", part)[0]]
-  esu_keys = local.esu_string == ""? [] : [for part in local.esu_parts : split(":", part)[1]]
-  esu = local.esu_string == ""? {} : zipmap(local.esu_keys, local.esu_values)
-  # Choose the existing uuid or a new one if it doesn't exist
-  secret_uuids                = { for env_name, secret_id in var.env_secrets : env_name => (lookup(local.esu, secret_id, "") == ""? "secret-${random_uuid.secrets[env_name].result}" : local.esu[secret_id])}
-  secret_mappings            = { for env_name, secret_id in var.env_secrets : env_name => "${local.secret_uuids[env_name]}:${secret_id}" }
-  run_googleapis_com_secrets = join(",", values(local.secret_mappings))
 }
